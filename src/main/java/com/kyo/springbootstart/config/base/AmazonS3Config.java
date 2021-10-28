df9b3c9decc8d0ec3c0af6d02296f331dce5cd15
김규호 <rlarbghrbgh@gmail.com>
2021-10-28 21:47:56 +0900

    aws s3 파일 업로드 api 개발

---
+++ src/main/java/com/kyo/springbootstart/config/base/AmazonS3Config.java
@@ -0,0 +1,40 @@
+package com.kyo.springbootstart.config.base;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.Locale;
+
+@Slf4j
+@Configuration
+public class AmazonS3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        log.info("amazon S3 accessKey => {}, secretKey => {}, region => {}", accessKey, secretKey, region);
+
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+
+
+
+}
--- src/main/java/com/kyo/springbootstart/process/base/controller/HateoasController.java
+++ src/main/java/com/kyo/springbootstart/process/base/controller/HateoasController.java
@@ -3,14 +3,20 @@ package com.kyo.springbootstart.process.base.controller;
 import com.kyo.springbootstart.config.exception.ApiException;
 import com.kyo.springbootstart.config.exception.ExceptionEnum;
 import com.kyo.springbootstart.process.base.dto.HateoasSample;
+import com.kyo.springbootstart.process.base.utils.S3Uploader;
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -29,8 +35,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
  *
  */
 @RestController
+@RequiredArgsConstructor
 public class HateoasController {
 
+    private final S3Uploader s3Uploader;
 
     // 단건
     @GetMapping(value = "/hateoas-test", produces = { "application/hal+json" })
@@ -78,6 +86,27 @@ public class HateoasController {
 
         return model;
 
+    }
+
+
+    @PostMapping(value = "/hateoas-test3", produces = { "application/hal+json" })
+    public EntityModel<HateoasSample> upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+
+        String name = s3Uploader.upload(multipartFile, "static");
+
+        HateoasSample sample = HateoasSample.builder()
+                .name(name)
+                .prefix("s3-test")
+                .build();
+
+        if (sample == null) {
+            throw new ApiException(ExceptionEnum.NOT_FOUND_EXCEPTION_01);
+        }
+
+        EntityModel<HateoasSample> model = EntityModel.of(sample);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).upload(multipartFile));
+        model.add(linkTo.withSelfRel());
+        return model;
     }
 
 
